6c8a6093b558acbe890ba518525fc3e0fb569c04
waseem mansha <waseem.mansha@gmail.com>
2021-10-08 15:15:08 +0500

    create folder structure

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/theme.js
@@ -0,0 +1,36 @@
+import { createTheme } from "@mui/material/styles";
+
+export const themes = ["light", "dark"].map((mode) =>
+  createTheme(
+    {
+      palette: {
+        mode,
+        primary: {
+          main: mode === "light" ? "rgb(24,119,242)" : "rgb(45,136,255)",
+        },
+        background: {
+          default: mode === "light" ? "rgb(240,242,245)" : "rgb(24,25,26)",
+        },
+      },
+
+      typography: {
+        fontFamily: [
+          `-apple-system`,
+          `"BlinkMacSystemFont"`,
+          `"Segoe UI"`,
+        ].join(","),
+      },
+    },
+    {
+      typography: {
+        h1: { fontSize: "2em" },
+        h2: { fontSize: "1.5em" },
+        h3: { fontSize: "1.3em" },
+        h4: { fontSize: "1em" },
+        h5: { fontSize: "0.8em" },
+        h6: { fontSize: "0.7em" },
+        button: { textTransform: "none" },
+      },
+    }
+  )
+);
