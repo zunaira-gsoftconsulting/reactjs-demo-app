8f3d3520a3dc01f30902e5c950ef07e2a2c3094f
sana-gsoft <sana@gsoftconsulting.com>
2021-10-11 18:34:24 +0500

    dashboard localization

--- src/components/Dashboard/chart.js
+++ src/components/Dashboard/chart.js
@@ -1,6 +1,4 @@
 import * as React from "react";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
 import { useStyles } from "./styles";
 import Chart from "react-apexcharts";
 import { Box, Card, CardHeader } from '@mui/material';
