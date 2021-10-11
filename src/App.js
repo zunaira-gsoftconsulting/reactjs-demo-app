1a4ae09d1cc5e655e378355f9e7a367def2011e3
sana-gsoft <sana@gsoftconsulting.com>
2021-10-11 14:33:30 +0500

    lint configurations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { useState } from "react";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/lib/integration/react";
-import storeConfig from "./redux/store";
-import { ThemeProvider } from "@mui/material/styles";
-import AppRouter from "./routes/index";
-import { themes } from "./utils/theme";
-function App() {
-  const { store, persistor } = storeConfig;
-  const [theme, setTheme] = useState(themes[0]);
+import React, { useState } from 'react'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import storeConfig from './redux/store'
+import { ThemeProvider } from '@mui/material/styles'
+import AppRouter from './routes/index'
+import { themes } from './utils/theme'
+function App () {
+  const { store, persistor } = storeConfig
+  const [theme, setTheme] = useState(themes[0])
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
@@ -16,7 +16,10 @@ function App() {
         </ThemeProvider>
       </PersistGate>
     </Provider>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
