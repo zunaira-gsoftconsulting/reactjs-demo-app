0419773d4965483aaabd6ef268f507d8873a113d
Zuni1 <zunaira@gsoftconsulting.com>
2022-09-01 15:31:33 +0500

    axios

--- src/redux/api/Index.js
+++ src/redux/api/Index.js
@@ -3,12 +3,19 @@ import { fetchRequest, fetchSuccess, fetchFailure } from "../reducers/auth";
 import { API_URL } from "../../utils/constants";
 
 const baseURL = process.env.NODE_ENV === "development" ? API_URL : API_URL;
+const token = localStorage.getItem("token");
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  },
+};
 
 export const fetchApi = () => {
   return function (dispatch) {
     dispatch(fetchRequest());
     axios
-      .get(baseURL)
+      .get(baseURL, config)
       .then((res) => {
         dispatch(fetchSuccess(res));
       })
